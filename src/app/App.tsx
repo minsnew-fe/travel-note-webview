ff5327b3163862ab9277e0bd3055ea45fe3c8c71
minsnew-fe <c.minsnew@gmail.com>
2024-08-08 20:19:48 +0900

    feat: router 내 map page 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import MainPage from "../pages/MainPage/MainPage";
+import MapPage from "../pages/MapPage/MapPage";
 
 const App = () => {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <MainPage />,
+    },
+    {
+      path: "/map",
+      element: <MapPage />,
     },
   ]);
 
---
+++ src/pages/MapPage/MapPage.tsx
@@ -0,0 +1,5 @@
+const MapPage = () => {
+  return <div>Map Page</div>;
+};
+
+export default MapPage;
