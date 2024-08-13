e60b7329d62a41339df350de9e433ed85683e320
minsnew-fe <c.minsnew@gmail.com>
2024-08-13 12:16:57 +0900

    feat: kakao map 적용

--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react";
+
 const MapPage = () => {
-  return <div>Map Page</div>;
+  const [kakaoMap, setKakaoMap] = useState<kakao.maps.Map>();
+
+  const initializeMap = () => {
+    const container = document.getElementById("map");
+
+    console.log("AAAa", window?.kakao?.maps);
+    const options = {
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+
+    if (container) {
+      const map = new window.kakao.maps.Map(container, options);
+      setKakaoMap(map);
+    }
+  };
+
+  useEffect(() => {
+    initializeMap();
+  }, [window.kakao]);
+
+  return (
+    <div>
+      <div>Map Page</div>
+      <div id="map" style={{ width: "500px", height: "500px" }} />
+    </div>
+  );
 };
 
 export default MapPage;
