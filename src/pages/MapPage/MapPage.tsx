139e8ee5f6e4f9ba3ff4543a19f38a60b5d10846
minsnew-fe <c.minsnew@gmail.com>
2024-08-22 12:44:13 +0900

    feat: naver map 표시 확인

--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -18,14 +18,27 @@ const MapPage = () => {
     }
   };
 
+  const initializeNaverMap = () => {
+    const container = document.getElementById("naver-map");
+    if (container) {
+      const location = new window.naver.maps.LatLng(33.450701, 126.570667);
+      const map = new window.naver.maps.Map(container, {
+        center: location,
+        zoom: 15, // 지도 확대 정도
+      });
+    }
+  };
+
   useEffect(() => {
     initializeMap();
+    initializeNaverMap();
   }, [window.kakao]);
 
   return (
     <div className="h-full w-screen">
       <div>Map Page</div>
-      <div id="map" className="w-full h-full" />
+      <div id="map" className="w-[500px] h-[500px]" />
+      <div id="naver-map" className="w-[500px] h-[500px]" />
     </div>
   );
 };
